b53b0e6d26a840e0bb948d445d838d1452eec7ea
AlekseyTn <tn.aleksey@gmail.com>
2021-01-26 23:21:51 +0300

    children change

--- app/src/main/java/com/example/othertestgit/Test.java
+++ app/src/main/java/com/example/othertestgit/Test.java
@@ -1,5 +1,17 @@
 package com.example.othertestgit;
 
 public class Test {
-    //test class
+    private int count;
+
+    public Test(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
